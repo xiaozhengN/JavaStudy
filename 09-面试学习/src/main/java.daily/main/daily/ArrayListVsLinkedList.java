aa4769b422b47c294462c7aee583744f51aad718
zbz <571457082@qq.com>
2022-10-07 17:01:59 +0800

    重命名部分文件

--- 09-面试学习/src/main/java.daily/main/daily/ArrayListVsLinkedList.java
+++ 09-面试学习/src/main/java.daily/main/daily/ArrayListVsLinkedList.java
@@ -12,37 +12,46 @@ import java.util.stream.Collectors;
 
 import static main.daily.Utils.randomArray;
 
-@SuppressWarnings("all")
+/**
+ * ArrayList与LinkedList比较
+ *
+ * @author xiaozhengN 571457082@qq.com
+ * @since 2022年10月7日00:47:41
+ **/
 public class ArrayListVsLinkedList {
     public static void main(String[] args) {
         int n = 1000;
-        int insertIndex = n;
-        for (int i = 0; i < 1; i++) {
-            int[] array = randomArray(n);
-            List<Integer> list1 = Arrays.stream(array).boxed().collect(Collectors.toList());
-            LinkedList<Integer> list2 = new LinkedList<>(list1);
-
-//            randomAccess(list1, list2, n / 2);
-//            addFirst(list1,list2);
-//            addMiddle(list1, list2, n / 2);
-//            addLast(list1,list2);
-            arrayListSize((ArrayList<Integer>) list1);
-            linkedListSize(list2);
-        }
+        int insertIndex = n / 2;
+        int[] array = randomArray(n);
+        List<Integer> list1 = Arrays.stream(array).boxed().collect(Collectors.toList());
+        LinkedList<Integer> list2 = new LinkedList<>(list1);
+        // 测试随机访问
+        randomAccess(list1, list2, insertIndex);
+        // 测试头部插入
+        addFirst(list1,list2);
+        // 测试中间插入
+        addMiddle(list1, list2, insertIndex);
+        // 测试尾部插入
+        addLast(list1,list2);
+        // 局部性原理, 空间占用测试
+        arrayListSize((ArrayList<Integer>) list1);
+        linkedListSize(list2);
     }
 
-    static void linkedListSize(LinkedList<Integer> list) {
+    private static void linkedListSize(LinkedList<Integer> list) {
         try {
+            System.out.println("局部性原理, 空间占用测试 LinkedList: ");
             long size = 0;
             ClassLayout layout = ClassLayout.parseInstance(list);
-//            System.out.println(layout.toPrintable());
             size += layout.instanceSize();
+
             Field firstField = LinkedList.class.getDeclaredField("first");
             firstField.setAccessible(true);
             Object first = firstField.get(list);
-//            System.out.println(ClassLayout.parseInstance(first).toPrintable());
             long nodeSize = ClassLayout.parseInstance(first).instanceSize();
+
             size += (nodeSize * (list.size() + 2));
+
             long elementSize = ClassLayout.parseInstance(list.getFirst()).instanceSize();
             System.out.println("LinkedList size:[" + size + "],per Node size:[" + nodeSize + "],per Element size:[" + elementSize * list.size() + "]");
         } catch (Exception e) {
@@ -50,17 +59,19 @@ public class ArrayListVsLinkedList {
         }
     }
 
-    static void arrayListSize(ArrayList<Integer> list) {
+    private static void arrayListSize(ArrayList<Integer> list) {
         try {
+            System.out.println("局部性原理, 空间占用测试 ArrayList: ");
             long size = 0;
             ClassLayout layout = ClassLayout.parseInstance(list);
-//            System.out.println(layout.toPrintable());
             size += layout.instanceSize();
+
             Field elementDataField = ArrayList.class.getDeclaredField("elementData");
             elementDataField.setAccessible(true);
             Object elementData = elementDataField.get(list);
-//            System.out.println(ClassLayout.parseInstance(elementData).toPrintable());
+
             size += ClassLayout.parseInstance(elementData).instanceSize();
+
             long elementSize = ClassLayout.parseInstance(list.get(0)).instanceSize();
             System.out.println("ArrayList size:[" + size + "],array length:[" + length(list) + "],per Element size:[" + elementSize * list.size() + "]");
         } catch (Exception e) {
@@ -68,59 +79,51 @@ public class ArrayListVsLinkedList {
         }
     }
 
-    static void randomAccess(List<Integer> list1, LinkedList<Integer> list2, int mid) {
-        StopWatch sw = new StopWatch();
-        sw.start("ArrayList");
+    private static void randomAccess(List<Integer> list1, LinkedList<Integer> list2, int mid) {
+        StopWatch sw = new StopWatch("测试随机访问");
+        sw.start("测试随机访问: ArrayList");
         list1.get(mid);
         sw.stop();
-
-        sw.start("LinkedList");
+        sw.start("测试随机访问: LinkedList");
         list2.get(mid);
         sw.stop();
-
         System.out.println(sw.prettyPrint());
     }
 
     private static void addMiddle(List<Integer> list1, LinkedList<Integer> list2, int mid) {
-        StopWatch sw = new StopWatch();
-        sw.start("ArrayList");
+        StopWatch sw = new StopWatch("测试中间插入");
+        sw.start("ArrayList: ");
         list1.add(mid, 100);
         sw.stop();
-
-        sw.start("LinkedList");
+        sw.start("LinkedList: ");
         list2.add(mid, 100);
         sw.stop();
-
         System.out.println(sw.prettyPrint());
     }
 
     private static void addFirst(List<Integer> list1, LinkedList<Integer> list2) {
-        StopWatch sw = new StopWatch();
-        sw.start("ArrayList");
+        StopWatch sw = new StopWatch("测试头部插入");
+        sw.start("ArrayList: ");
         list1.add(0, 100);
         sw.stop();
-
-        sw.start("LinkedList");
+        sw.start("LinkedList: ");
         list2.addFirst(100);
         sw.stop();
-
         System.out.println(sw.prettyPrint());
     }
 
     private static void addLast(List<Integer> list1, LinkedList<Integer> list2) {
-        StopWatch sw = new StopWatch();
-        sw.start("ArrayList");
+        StopWatch sw = new StopWatch("测试尾部插入");
+        sw.start("ArrayList: ");
         list1.add(100);
         sw.stop();
-
-        sw.start("LinkedList");
+        sw.start("LinkedList: ");
         list2.add(100);
         sw.stop();
-
         System.out.println(sw.prettyPrint());
     }
 
-    public static int length(ArrayList<Integer> list) {
+    private static int length(ArrayList<Integer> list) {
         try {
             Field field = ArrayList.class.getDeclaredField("elementData");
             field.setAccessible(true);
