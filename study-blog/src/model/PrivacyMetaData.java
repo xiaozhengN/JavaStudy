e9829e9218e92cb9b8b391858c36675f2baa366d
zbz <571457082@qq.com>
2023-05-31 23:05:22 +0800

    记录一次修改Commons导致的BUG

--- study-blog/src/model/PrivacyMetaData.java
+++ study-blog/src/model/PrivacyMetaData.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * PrivacyMetaData模型
  *
@@ -36,6 +40,11 @@ public class PrivacyMetaData {
      */
     private AppStatusEnum appStatus;
 
+    /**
+     * 扩展字段
+     */
+    private Map<String, String> extend;
+
     @AllArgsConstructor
     @NoArgsConstructor
     public enum AppStatusEnum {
@@ -52,5 +61,25 @@ public class PrivacyMetaData {
          * 状态名称
          */
         private String statusName;
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        PrivacyMetaData privacyMetaData = new PrivacyMetaData();
+        Map<String, String> extndMap = new HashMap<>();
+        extndMap.put("1", "1");
+        extndMap.put("2", "2");
+        privacyMetaData.setExtend(extndMap);
+        Field[] fields = privacyMetaData.getClass().getDeclaredFields();
+        // 判断某个对象是否有指定字段
+        for (Field field : fields) {
+            if (field.getName().equals("extend")) {
+                Field declaredField = privacyMetaData.getClass().getDeclaredField("extend");
+                declaredField.setAccessible(true);
+                Object extendObj = declaredField.get(privacyMetaData);
+                if (extendObj instanceof Map) {
+                    Map<Object, Object> extendObj1 = (Map<Object, Object>) extendObj;
+                }
+            }
+        }
     }
 }
